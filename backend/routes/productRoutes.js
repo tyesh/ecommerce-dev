2e64acb79aecce394ea841fa08d066e7e635516c
carlos <carlos.velazquez91@gmail.com>
2022-05-28 08:16:45 -0400

    Added product routes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
+const router = express.Router();
+import Product from '../models/ProductModel.js';
+
+// @desc Fetch all products
+// @route GET /api/products
+// æaccess Public
+router.get(
+  '/',
+  expressAsyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
+
+// @desc Fetch single product
+// @route GET /api/product/:id
+// æaccess Public
+router.get(
+  '/:id',
+  expressAsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: 'Product notfound' });
+    }
+  })
+);
+
+export default router;

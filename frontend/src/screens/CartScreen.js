b4fd1cf32d0fd4fa7c1f048f3822478a74347f43
carlos <carlos.velazquez91@gmail.com>
2022-05-31 15:15:57 -0400

    Added cartScreen content

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -24,13 +24,95 @@ const CartScreen = () => {
 
   const dispatch = useDispatch();
 
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   useEffect(() => {
     if (productId) {
       dispatch(addToCart(productId, qty));
     }
   }, [dispatch, productId, qty]);
 
-  return <div>CartScreen</div>;
+  const removeFromCartHandler = (id) => {
+    console.log('removed');
+  };
+
+  const checkoutHandler = () => {
+    console.log('checkout');
+  };
+
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Carrito de compras</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Tu carrito està vacìo. <Link to='/'>Atràs</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>{item.price}Gs</Col>
+                  <Col md={2}>
+                    <Form.Control
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    />
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type='button'
+                      variant='light'
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className='fas fa-trash' />
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2 className='h3'>
+                Subtotal (
+                {cartItems.reduce((acc, item) => acc + parseInt(item.qty), 0)})
+                items
+              </h2>
+              {cartItems.reduce((acc, item) => acc + item.qty * item.price, 0)}
+              Gs
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type='button'
+                className='btn-block'
+                disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
+              >
+                Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
+  );
 };
 
 export default CartScreen;

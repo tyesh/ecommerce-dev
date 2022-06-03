a9d7b0ce5888a076e1590337bab1183d95ab05d9
carlos <carlos.velazquez91@gmail.com>
2022-06-03 15:49:14 -0400

    Aded password encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -45,4 +45,36 @@ const getUserProfile = expressAsyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+// @desc    Register a new user
+// @route   POST /api/users
+// æaccess  public
+const registerUser = expressAsyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.emai,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user Data');
+  }
+});
+
+export { authUser, getUserProfile, registerUser };

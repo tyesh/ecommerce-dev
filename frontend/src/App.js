99e07ee8df15136d43b5544732e705028608899f
unknown <carlos.velazquez91@gmail.com>
2022-06-21 16:12:02 -0400

    Admin Product  List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
+import ProductListScreen from './screens/ProductListScreen';
 
 const App = () => {
   return (
@@ -39,6 +40,10 @@ const App = () => {
               <Route path='/order/:id' element={<OrderScreen />} />
               <Route path='/admin/userList' element={<UserListScreen />} />
               <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
+              <Route
+                path='/admin/productsList'
+                element={<ProductListScreen />}
+              />
             </Routes>
           </Container>
         </main>
